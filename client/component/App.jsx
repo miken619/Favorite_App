cadcbbbe36c71fe501787b66caf9b4b6eccfc6ea
Hack Reactor Student <student@hackreactor.com>
2017-08-01 08:29:01 -0700

    Added a stylesheet and created empty components

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const ToDoList = () => {
-  return (
-    <div>
+
+import User from './User.jsx';
+import Content from './Content.jsx';
+import Submission from './Submission.jsx';
+
+
+export default class App extends {
+
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+  
+  }
+
+  handleSubmission() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <User />
+        </div>
+        <div>
+          <div> 
+            <Content />
+          </div>
+          <div>
+            <Submission />
+          </div>
+        </div>
+   
+      </div>
       
-    </div>
-  );
-
-};
+    )
+  }
+}
